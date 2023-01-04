ca94626e2558be36009292dcceb61a2dda620a98
rosen-vladimirov <rosen.vladimirov@progress.com>
2023-01-04 22:12:54 +0200

    fix: use mocha for integration tests

--- integration-tests/linux.js
+++ integration-tests/linux.js
@@ -1,4 +1,15 @@
-const pathToPackage = require("../").getPath("local-module", "local-module");
-const pathToPackageLm = require("../").getPath("local-module", "lm");
-console.log("PATH TO PACKAGE: " + pathToPackage);
-console.log("PATH TO PACKAGELM " + pathToPackageLm);
+const { assert } = require("chai");
+
+describe("integration tests", () => {
+    it("full name", () => {
+
+        const pathToPackage = require("../").getPath("local-module", "local-module");
+        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
+    });
+
+    it("short name", () => {
+
+        const pathToPackage = require("../").getPath("local-module", "lm");
+        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
+    });
+});
--- integration-tests/linux.sh
+++ integration-tests/linux.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/bash
 
-cd local-module
-npm pack
-cd ../
 npm i -g ./local-module/local-module-1.0.0.tgz
-pwd
-node linux.js
+../node_modules/mocha/bin/_mocha --recursive --reporter spec-xunit-file --timeout 1500 linux.js
