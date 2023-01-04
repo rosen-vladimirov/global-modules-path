4fde45920a7a00f3bfc2607a9d42950bc4ab0932
rosen-vladimirov <rosen.vladimirov@progress.com>
2023-01-04 22:15:29 +0200

    fix: add missing / in the integration tests

--- integration-tests/linux.js
+++ integration-tests/linux.js
@@ -2,14 +2,12 @@ const { assert } = require("chai");
 
 describe("integration tests", () => {
     it("full name", () => {
-
         const pathToPackage = require("../").getPath("local-module", "local-module");
-        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
+        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "/node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
     });
 
     it("short name", () => {
-
         const pathToPackage = require("../").getPath("local-module", "lm");
-        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
+        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "/node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
     });
 });
