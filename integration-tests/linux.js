98a2d35fd8868a0050bc5ea69453e4628cae828e
rosen-vladimirov <rosen.vladimirov@progress.com>
2023-01-08 17:06:35 +0200

    chore: try setup on windows

--- integration-tests/linux.js
+++ integration-tests/linux.js
@@ -1,13 +1,18 @@
 const { assert } = require("chai");
 
-describe("integration tests", () => {
-    it("full name", () => {
+describe("Integration tests", () => {
+    it("finds correct path to executable when full name of executable is used", () => {
         const pathToPackage = require("../").getPath("local-module", "local-module");
         assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "/node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
     });
 
-    it("short name", () => {
+    it("finds correct path to executable when package name and executable name differ", () => {
         const pathToPackage = require("../").getPath("local-module", "lm");
+        assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "/node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
+    });
+
+    it("finds correct path to executable when only package name is used", () => {
+        const pathToPackage = require("../").getPath("local-module");
         assert.deepEqual(pathToPackage.replace(/\/node\/18\..+?\..+?\//, "/node/NODE_VERSION/"), "/opt/hostedtoolcache/node/NODE_VERSION/x64/lib/node_modules/local-module")
     });
 });
